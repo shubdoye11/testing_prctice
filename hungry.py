13bd03b98b4513f83872d63b6c72d270f85c44e6
Shubham Doye <doye11shubham@gmail.com>
2022-10-04 22:08:38 +0530

    thisty committed

--- hungry.py
+++ hungry.py
@@ -3,4 +3,5 @@ hungry = input("are you hungry?")
 if hungry == "yes" :
     print("eat something and resume the game.")
 else:
+    print("WATER WATER DRINK")
     print("keep playing until you win the game")

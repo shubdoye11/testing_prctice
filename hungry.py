7be723dcc4e1c0603a596004e10909b5c0f42dd5
Shubham Doye <doye11shubham@gmail.com>
2022-10-04 09:08:00 +0530

    2nd version commited file

--- hungry.py
+++ hungry.py
@@ -1,1 +1,6 @@
-print("i am hungry")
+hungry = input("are you hungry?")
+
+if hungry == "yes" :
+    print("eat something and resume the game.")
+else:
+    print("keep playing until you win the game")

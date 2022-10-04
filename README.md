48df03ec3e9c8400bb8bee5ac5dc375116d8e3a0
Shubham Doye <doye11shubham@gmail.com>
2022-10-04 09:42:46 +0530

    3rd edit commited

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # testing_prctice
 learning from code basics regarding git and github
 its been few finutes , i have been practicing on github
+
+gym day
+ruuning day
--- hungry.py
+++ hungry.py
@@ -2,5 +2,7 @@ hungry = input("are you hungry?")
 
 if hungry == "yes" :
     print("eat something and resume the game.")
+    print('keep drinking water')
+    print('eat fruits')
 else:
     print("keep playing until you win the game")

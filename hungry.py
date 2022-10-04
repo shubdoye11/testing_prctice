51c97187fc871a8398e7af0faba392a71c4ef9a8
Shubham Doye <doye11shubham@gmail.com>
2022-10-04 09:50:39 +0530

    jksdfh

--- hungry.py
+++ hungry.py
@@ -2,7 +2,5 @@ hungry = input("are you hungry?")
 
 if hungry == "yes" :
     print("eat something and resume the game.")
-    print('keep drinking water')
-    print('eat fruits')
 else:
     print("keep playing until you win the game")
